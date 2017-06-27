85ac33e69af6f1c24ef14091c9b599f909190eaa
yungpanko <jrjohns@mit.edu>
2017-06-27 15:29:05 -0400

    Done.

--- index.js
+++ index.js
@@ -1,4 +1,22 @@
 function countdown(callback) {
-  window.setTimeout(2);
-  callback();
+  window.setTimeout(function() {
+    callback()
+  }, 2000);
 }
+
+function createMultiplier(multiplierValue) {
+  return function(number) {
+    return number * multiplierValue;
+  }
+}
+
+let doubler = createMultiplier(2);
+let tripler = createMultiplier(3);
+
+function multiplier(number, multiplierValue) {
+  return number * multiplierValue;
+}
+
+
+let doublerWithBind = multiplier.bind(null, 2)
+let triplerWithBind = multiplier.bind(null, 3)
